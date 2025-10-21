7641d0148efc35a54188fd653f2405a787ca4ef1
EthanMTift <ethanmtift@gmail.com>
2025-10-20 22:41:33 -0700

    Added energyBug, to help with reporting the bug to factorio forums. Deleted the useless log from PromethiumTech. Fixed fluid wagon not scaling with quality because i wasnt referencing the correct quality affects capacity hook

--- PromethiumFuel/data.lua
+++ PromethiumFuel/data.lua
@@ -3,4 +3,4 @@ require("prototypes.recipe")
 require("prototypes.technology")
 
 data.raw["cargo-wagon"]["cargo-wagon"].quality_affects_inventory_size = true
-data.raw["fluid-wagon"]["fluid-wagon"].quality_affects_inventory_size = true
+data.raw["fluid-wagon"]["fluid-wagon"].quality_affects_capacity = true
---
+++ energyBug/data.lua
@@ -0,0 +1,7 @@
+data.raw["reactor"]["heating-tower"].energy_source = {
+    type = "electric",
+    buffer_capacity = "1000MW",
+    usage_priority = "secondary-input",
+    input_flow_limit = "1000MW",
+    output_flow_limit = "1000MW",
+}
