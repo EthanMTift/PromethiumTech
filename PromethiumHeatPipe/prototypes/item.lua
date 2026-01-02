0f7e5042a2a3120039adae68b587d3cee9e06e55
EthanMTift <ethanmtift@gmail.com>
2026-01-02 14:20:26 -0800

    added pictures to the frozen biter eggs so they dont look like normal ones when on a belt

--- PromethiumHeatPipe/prototypes/item.lua
+++ PromethiumHeatPipe/prototypes/item.lua
@@ -14,6 +14,32 @@ frozen_biter_egg.name = "frozen-biter-egg"
 frozen_biter_egg.icon = "__PromethiumHeatPipe__/graphics/frozen-biter-egg.png"
 frozen_biter_egg.icon_size = 64
 frozen_biter_egg.spoil_ticks = 540000
+frozen_biter_egg.pictures = {
+    {
+      filename = "__PromethiumHeatPipe__/graphics/frozen-biter-egg.png",
+      mipmap_count = 4,
+      scale = 0.5,
+      size = 64
+    },
+    {
+      filename = "__PromethiumHeatPipe__/graphics/frozen-biter-egg-1.png",
+      mipmap_count = 4,
+      scale = 0.5,
+      size = 64
+    },
+    {
+      filename = "__PromethiumHeatPipe__/graphics/frozen-biter-egg-2.png",
+      mipmap_count = 4,
+      scale = 0.5,
+      size = 64
+    },
+    {
+      filename = "__PromethiumHeatPipe__/graphics/frozen-biter-egg-3.png",
+      mipmap_count = 4,
+      scale = 0.5,
+      size = 64
+    }
+  },
 
 
 data:extend({frozen_biter_egg})
