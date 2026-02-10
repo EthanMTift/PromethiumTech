499b121f04b139ce6b1bcc8d5617c0e220e36d53
EthanMTift <ethanmtift@gmail.com>
2026-02-10 13:02:15 -0800

    added spoil timers to prom science and prom asteroids, and increased the durability of prom science to 2 times. This should make for a more interesting logistical challenge that rewards speedy ships and punishes belt stacking, while allowing for less UPS burden due to the potential for increased potency with the prom science packs

--- PromethiumHeatPipe/data.lua
+++ PromethiumHeatPipe/data.lua
@@ -2,3 +2,7 @@ require("prototypes.item")
 require("prototypes.entity")
 require("prototypes.recipe")
 require("prototypes.technology")
+
+data.raw["tool"]["promethium-science-pack"].spoil_ticks = 1000000000
+data.raw["tool"]["promethium-science-pack"].durability = 2
+data.raw["item"]["promethium-asteroid-chunk"].spoil_ticks = 108000
