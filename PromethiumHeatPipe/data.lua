e291466823f559b3097b0dcc3536bf7cda5a1601
EthanMTift <157545170+EthanMTift@users.noreply.github.com>
2026-02-10 14:26:05 -0800

    Update data.lua
    Quadrupled the promethium asteroid spoiler timer so that belt ships are nerfed instead of completely worthless.

--- PromethiumHeatPipe/data.lua
+++ PromethiumHeatPipe/data.lua
@@ -5,4 +5,4 @@ require("prototypes.technology")
 
 data.raw["tool"]["promethium-science-pack"].spoil_ticks = 1000000000
 data.raw["tool"]["promethium-science-pack"].durability = 2
-data.raw["item"]["promethium-asteroid-chunk"].spoil_ticks = 108000
+data.raw["item"]["promethium-asteroid-chunk"].spoil_ticks = 432000
