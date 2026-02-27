5cd024c327cc78a336c4f35cab0384a398338431
EthanMTift <ethanmtift@gmail.com>
2026-02-27 11:39:37 -0800

    made promethium soil able to be placed on all nauvis tiles and only nauvis tiles, still need to get it to remove decoratives

--- PromethiumSoil/prototypes/treeitem.lua
+++ PromethiumSoil/prototypes/treeitem.lua
@@ -2,6 +2,35 @@ local treeItem = table.deepcopy(data.raw.item["landfill"])
 
 treeItem.name = "promethium-compost"
 treeItem.place_as_tile.result = "promethium-compost"
+treeItem.place_as_tile.condition = { layers = {} }
+treeItem.place_as_tile.tile_condition = {
+  "water",
+  "deepwater",
+  "water-green",
+  "deepwater-green",
+  "water-shallow",
+  "nuclear-ground",
+  "red-desert-0",
+  "red-desert-1",
+  "red-desert-2",
+  "red-desert-3",
+  "sand-1",
+  "sand-2",
+  "sand-3",
+  "dirt-1",
+  "dirt-2",
+  "dirt-3",
+  "dirt-4",
+  "dirt-5",
+  "dirt-6",
+  "dirt-7",
+  "dry-dirt"
+  
+
+  
+
+}
+
   
 
 treeItem.icons = {
--- PromethiumSoil/prototypes/treetile.lua
+++ PromethiumSoil/prototypes/treetile.lua
@@ -4,5 +4,6 @@ treeTile.name = "promethium-compost"
 treeTile.map_color = {48, 19, 0}
 treeTile.tint = {140, 80, 6, 1}
 treeTile.minable.result = "promethium-compost"
+treeTile.decorative_removal_probability = 1.0
 
 data.extend{treeTile}
