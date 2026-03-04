a425589d6fc278f670eaf408d6ef0898b85ed5c0
EthanMTift <ethanmtift@gmail.com>
2026-03-04 14:31:27 -0800

    Changed science pack durability back to one, increased spoil time to the max for the pack, and changed the recipe to require only 12 promethium chunks instead of 25. This should have the same effect of reducing the ups cost of promethium science, while also making egg spoilage more impactful towards the outcomes freshness

--- PromethiumHeatPipe/data.lua
+++ PromethiumHeatPipe/data.lua
@@ -2,7 +2,10 @@ require("prototypes.item")
 require("prototypes.entity")
 require("prototypes.recipe")
 require("prototypes.technology")
-
-data.raw["tool"]["promethium-science-pack"].spoil_ticks = 1000000000
-data.raw["tool"]["promethium-science-pack"].durability = 2
+data.raw["tool"]["promethium-science-pack"].ingredients = {
+    { type = "item",  name = "biter-egg", amount = 10 },
+    { type = "item",  name = "quantum-processor", amount = 1 },
+    { type = "item",  name = "promethium-chunk", amount = 12 },
+}
+data.raw["tool"]["promethium-science-pack"].spoil_ticks = 4294967295
 data.raw["item"]["promethium-asteroid-chunk"].spoil_ticks = 216000
