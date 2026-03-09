8347badc1a662f8b7042ff55932379cf4645cae3
EthanMTift <ethanmtift@gmail.com>
2026-03-09 13:06:33 -0700

    Fixed compost not displacing some tiles above it and looking ugly

--- PromethiumSoil/prototypes/treetile.lua
+++ PromethiumSoil/prototypes/treetile.lua
@@ -1,9 +1,9 @@
-local treeTile = table.deepcopy(data.raw.tile["landfill"])
-
+--local treeTile = table.deepcopy(data.raw.tile["landfill"])
+local treeTile = table.deepcopy(data.raw.tile["refined-concrete"])
 treeTile.name = "promethium-compost"
 treeTile.map_color = {48, 19, 0}
 treeTile.tint = {140, 80, 6, 1}
 treeTile.minable.result = "promethium-compost"
-treeTile.decorative_removal_probability = 1.0
+treeTile.variants = data.raw.tile["landfill"].variants
 
 data.extend{treeTile}
