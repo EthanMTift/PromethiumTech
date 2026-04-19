40bd53b8a4d0b2bd6a2b1623dda961875a34de14
EthanMTift <ethanmtift@gmail.com>
2026-04-19 15:54:18 -0700

    made conduit into an electric pole

--- PromethiumAge/prototypes/entity.lua
+++ PromethiumAge/prototypes/entity.lua
@@ -8733,23 +8733,8 @@ map_color = {78, 78, 78},
 data:extend({promethium_cargo_hub})
 
 -- PromethiumConduit --
-local conduit_entity = table.deepcopy(data.raw["lightning-attractor"]["lightning-collector"])
-conduit_entity.efficiency = 0.6
-conduit_entity.lightning_strike_offset = {
--0.7,
--2.5999999999999998
-}
-conduit_entity.resistances = {
-{
-percent = 90,
-type = "fire"
-},
-{
-percent = 100,
-type = "electric"
-}
-}
-conduit_entity.range_elongation = 30
+local conduit_entity = table.deepcopy(data.raw["electric-pole"]["substation"])
+
 
 conduit_entity.name = "promethium-conduit"
 conduit_entity.icon = "__PromethiumAge__/graphics/icons/promethium-conduit.png"
@@ -8760,119 +8745,81 @@ conduit_entity.corpse = "accumulator-remnants"
 conduit_entity.map_color = {97, 245, 255}
 conduit_entity.next_upgrade = nil 
 conduit_entity.flags = {"placeable-neutral", "placeable-player", "player-creation"}
-conduit_entity.collision_box = {{-1.5, -1.5}, {1.8, 1.8}}
-conduit_entity.selection_box = {{-1.8, -1.8}, {2.0, 2.0}}
-conduit_entity.surface_conditions = {
-{property = "magnetic-field", min = 99, max = 99}
-}
-
-
-
-
--- Energy properties: much higher capacity and flow limits
-conduit_entity.energy_source = {
-type = "electric",
-buffer_capacity = "1500MJ",
-usage_priority = "primary-output",
-output_flow_limit = "1500MW",
-drain = "5MW"
-}
-
--- === ChargableGraphics ===
-conduit_entity.chargable_graphics = {
-picture = {
-layers = {
+conduit_entity.collision_box = {{-1.3, -1.3}, {1.3, 1.3}}
+conduit_entity.selection_box = {{-1.5, -1.5}, {1.5, 1.5}}
+conduit_entity.connection_points = {
   {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png",
-    priority = "high",
-    width = 200,
-    height = 290,
-    scale = 0.50,
-    frame_count = 1,
-    repeat_count = 60,
-    shift = {0, 0},
-    stripes = {
-      {filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png", width_in_frames = 10, height_in_frames = 6},
+    shadow = {
+      copper = {
+        3.078125,
+        0.078125
+      },
+      green = {
+        2.671875,
+        0.125
+      },
+      red = {
+        3.46875,
+        0.140625
+      }
     },
+    wire = {
+      copper = {
+        0,
+        -2.578125
+      },
+      green = {
+        -0.390625,
+        -2.53125
+      },
+      red = {
+        0.40625,
+        -2.53125
+      }
+    }
   },
-  {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-hr-shadow.png",
-    priority = "high",
-    width = 600,
-    height = 400,
-    draw_as_shadow = true,
-    scale = 0.5,
-    shift = {0, 0},
+}
+conduit_entity.supply_area_distance = 31.5
+conduit_entity.maximum_wire_distance = 64
+conduit_entity.pictures = {
+  layers = {
+    {
+      filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png",
+      priority = "high",
+      direction_count = 1,
+      width = 200,
+      height = 290,
+      scale = 0.50,
+      frame_count = 1,
+      repeat_count = 60,
+      shift = {0, 0},
+      stripes = {
+        {filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png", width_in_frames = 10, height_in_frames = 6},
+      },
+    },
+    {
+      filename = "__PromethiumAge__/graphics/entity/promethium-conduit-hr-shadow.png",
+      priority = "high",
+      width = 600,
+      height = 400,
+      direction_count = 1,
+      draw_as_shadow = true,
+      scale = 0.5,
+      shift = {0, 0},
+    }
   }
-}
 },
-charge_animation = {
-layers = {
-  {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png",
-    priority = "high",
-    width = 200,
-    height = 290,
-    frame_count = 60,
-    line_length = 10,
-    scale = 0.5,
-    shift = {0, 0},
-  },
-  {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-emission.png",
-    priority = "high",
-    width = 200,
-    height = 290,
-    frame_count = 60,
-    line_length = 10,
-    scale = 0.5,
-    shift = {0, 0},
-    draw_as_glow = true,
-    blend_mode = "additive"
-  }
-}
-},
-discharge_animation = {
-layers = {
-  {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-animation.png",
-    priority = "high",
-    width = 200,
-    height = 290,
-    frame_count = 60,
-    line_length = 10,
-    shift = {0, 0},
-    scale = 0.5
-  },
-  {
-    filename = "__PromethiumAge__/graphics/entity/promethium-conduit-emission.png",
-    priority = "high",
-    width = 200,
-    height = 290,
-    frame_count = 60,
-    line_length = 10,
-    shift = {0, 0},
-    scale = 0.5,
-    draw_as_glow = true,
-    blend_mode = "additive"
-  }
-}
-},
-charge_cooldown = 30,
-discharge_cooldown = 30
-}
 
--- Sound
-conduit_entity.working_sound = {
-sound = {filename = "__base__/sound/accumulator-working.ogg", volume = 0.8},
-idle_sound = {filename = "__base__/sound/accumulator-idle.ogg", volume = 0.6},
-apparent_volume = 1.5
-}
 
-conduit_entity.factoriopedia_simulation = {
-init = "    require(\"__core__/lualib/story\")\n    game.simulation.camera_zoom = 1.2    game.simulation.camera_position = {0, -2}\n    game.surfaces[1].create_entity{name = \"promethium-conduit\", position = {0, 0}}\n    local story_table =\n    {\n      {\n        {\n          name = \"start\",\n          action = function() game.surfaces[1].execute_lightning{name = \"lightning\", position = {0, 2}} end\n        },\n        {\n          condition = story_elapsed_check(1),\n          action = function() story_jump_to(storage.story, \"start\") end\n        }\n      }\n    }\n    tip_story_init(story_table)\n  ",
-planet = "fulgora"
-},
+
+
+
+
+
+
+
+
+
 
 data:extend({conduit_entity})
 
--- PromethiumAge/prototypes/item.lua
+++ PromethiumAge/prototypes/item.lua
@@ -52,12 +52,12 @@ data:extend({fuel_item})
 
 
 -- PromethiumConduit --
-local conduit_item = table.deepcopy(data.raw["item"]["accumulator"])
+local conduit_item = table.deepcopy(data.raw["item"]["substation"])
 
 conduit_item.name = "promethium-conduit"
 conduit_item.icon = "__PromethiumAge__/graphics/icons/promethium-conduit.png"
 conduit_item.icon_size = 64
-conduit_item.subgroup = "energy"
+
 conduit_item.place_result = "promethium-conduit"
 conduit_item.stack_size = 5
 conduit_item.weight = 1000 * kg
